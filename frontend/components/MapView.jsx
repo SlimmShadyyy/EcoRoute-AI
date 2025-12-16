049207aa7ef9b31ab27d80d5648177db530a3b22
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-16 23:55:06 +0530

    Update MapView.jsx

--- frontend/components/MapView.jsx
+++ frontend/components/MapView.jsx
@@ -11,6 +11,20 @@ import {
 import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+
 function FitBounds({ coordinates, onMapReady }) {
   const map = useMap();
 
